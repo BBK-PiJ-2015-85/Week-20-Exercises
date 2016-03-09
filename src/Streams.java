5d91bb86eec471b9a36628e7bb9229317fb751f1
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2016-03-09 14:18:59 +0000

    added questions 6 -11

--- src/Streams.java
+++ src/Streams.java
@@ -1,8 +1,22 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Streams {
+
+    //question 10 method
+    public static List randomNumberList(int size) {
+        return new Random().doubles(size, 0, 1).boxed().collect(Collectors.toList());
+    }
+
+    //question 11 method
+    public static List orderedNumberList(int start, int step, int size) {
+        return IntStream.iterate(start, i -> i + step).limit(size).boxed().collect(Collectors.toList());
+    }
+
     public static void main(String[] args) {
         List<String> words = Arrays.asList("hi", "hello", "tonight", "once", "forever", "midnight",
                                             "black", "white", "back", "yellow", "sun", "red");
@@ -40,13 +54,47 @@ public class Streams {
         System.out.println();
 
         System.out.println("*****Question 5*****");
-        String q5 = words.stream().map(String::toUpperCase).filter((s) -> s.length() < 4)
+        System.out.println(words.stream().map(String::toUpperCase).filter((s) -> s.length() < 4)
                                                             .filter((s) -> s.contains("E"))
-                                                            .findFirst().orElse(null);
-        if (q5 != null) {
-            System.out.println(q5);
-        } else {
-            System.out.println("Result was null.");
-        }
+                                                            .findFirst().orElse("Not found."));
+
+
+        System.out.println(words.stream().map(String::toUpperCase).filter((s) -> s.length() < 4)
+                                                                    .filter((s) -> s.contains("Q"))
+                                                                    .findFirst().orElse("Not found."));
+        System.out.println();
+
+
+        System.out.println("*****Question 6*****");// NOT IMPLEMENTED
+        /*
+        List<String> upperCaseList = new ArrayList<>();
+        System.out.printf(words.stream().map(String::toUpperCase).filter((s) -> s.length() < 4)
+                                                                    .filter((s) -> s.contains("E"))
+                                                                     .findFirst().orElse(null));
+        */
+        System.out.println();
+
+        System.out.println("*****Question 7*****");
+        System.out.println(words.stream().reduce("", (s, t) -> (s + t).toUpperCase()));
+        System.out.println();
+
+        System.out.println("*****Question 8*****");
+        System.out.println(words.stream().map(String::toUpperCase).reduce("", String::concat));
+        System.out.println();
+
+        System.out.println("*****Question 9*****");
+        String q9 = words.stream().reduce((s, t) -> s + "," + t).orElse("need two strings.");
+        System.out.println(q9);
+        System.out.println();
+
+        System.out.println("*****Question 10*****");
+        System.out.println(Streams.randomNumberList(5));
+        System.out.println(Streams.randomNumberList(10));
+
+        System.out.println();
+
+        System.out.println("*****Question 11*****");
+        System.out.println(Streams.orderedNumberList(1, 1, 5));
+        System.out.println(Streams.orderedNumberList(10, 10, 10));
     }
 }
